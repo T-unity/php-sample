bf11e3a9adc597978a40b52073462b6b53234c91
T-unity <takadanaritoshi@gmail.com>
2021-03-08 18:34:09 +0900

    fixed miss

--- www/html/views/index.php
+++ www/html/views/index.php
@@ -3,8 +3,9 @@
     <label for="view_name">表示名</label>
     <input id="view_name" type="text" name="view_name" value="">
   </div>
+  <div>
     <label for="message">ひとことメッセージ</label>
     <textarea name="message" id="message" cols="30" rows="10"></textarea>
-  <div>
   </div>
+  <input type="submit" name="btn_submit" value="書き込む">
 </form>
