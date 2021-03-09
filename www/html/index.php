03233f6d48bf3447e822b4ea64da17a0ff561ec8
T-unity <takadanaritoshi@gmail.com>
2021-03-10 00:07:50 +0900

    refactoring

--- www/html/index.php
+++ www/html/index.php
@@ -1,36 +1,5 @@
 <?php
 
-$title = '一言掲示板';
+$title = 'ひとこと掲示板';
 $content = __DIR__ . '/views/index.php';
 include __DIR__ . '/views/layout.php';
-
-define('FILENAME', 'message.txt');
-
-// 変数の初期化
-$now_date = null;
-$data = null;
-$file_handle = null;
-
-if ( !empty($_POST['btn_submit']) ) {
-  if ( $file_handle = fopen( FILENAME, "a") ) {
-
-    // 書き込み日時の取得
-    $now_date = date("Y-m-d H:i:s");
-
-    // 書き込むデータを作成
-    $data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
-
-    // 書き込みを実行
-    fwrite($file_handle, $data);
-
-    // ファイルを閉じる
-    fclose( $file_handle );
-  }
-}
-
-if ($file_handle = fopen(FILENAME,'r')) {
-  while($data = fgets($file_handle)) {
-    echo $data . "<br>";
-  }
-  fclose($file_handle);
-}
--- www/html/views/index.php
+++ www/html/views/index.php
@@ -1,3 +1,49 @@
+<?php
+
+define('FILENAME', 'message.txt');
+
+// 変数の初期化
+$now_date = null;
+$data = null;
+$file_handle = null;
+// メッセージを配列に格納
+$split_data = null;
+$message = array();
+$message_array = array();
+
+// 入力データの書き込み
+if ( !empty($_POST['btn_submit']) ) {
+  if ( $file_handle = fopen( FILENAME, "a") ) {
+    // 書き込み日時の取得
+    $now_date = date("Y-m-d H:i:s");
+    // 書き込むデータを作成
+    $data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
+    // 書き込みを実行
+    fwrite($file_handle, $data);
+    // ファイルを閉じる
+    fclose( $file_handle );
+  }
+}
+
+// 表示部分
+if ($file_handle = fopen(FILENAME,'r')) {
+  while($data = fgets($file_handle)) {
+    $split_data = preg_split('/\'/', $data);
+
+    $message = array(
+      'view_name' => $split_data[1],
+      'message' => $split_data[3],
+      'post_date' => $split_data[5],
+    );
+    array_unshift($message_array, $message);
+
+    // echo $data . "<br>";
+  }
+  fclose($file_handle);
+}
+
+?>
+
 <form action="" method="post">
   <div>
     <label for="view_name">表示名</label>
@@ -9,3 +55,17 @@
   </div>
   <input type="submit" name="btn_submit" value="書き込む">
 </form>
+<hr>
+<section>
+  <?php if (!empty($message_array)): ?>
+    <?php foreach($message_array as $value): ?>
+      <article>
+        <div class="info">
+          <h2><?php echo $value['view_name']; ?></h2>
+          <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?>
+        </div>
+        <p><?php echo $value['message']; ?></p>
+      </article>
+    <?php endforeach; ?>
+  <?php endif; ?>
+</section>
